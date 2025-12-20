52594b93e0de494cc793afeea71ab2f5f007dbbb
unknown <Fayaj Hossain>
2025-12-20 12:32:20 +0530

    Initial commit: Core KVM orchestration engine

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Shaman786/vps-manager
+
+go 1.25.5
---
+++ internal/utils/download.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func DownloadFile(url string, filepath string) error {
+	out, err :=
+		os.Create(filepath)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("failed to download from %s: %w", url, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to save file: %w", err)
+	}
+	return nil
+}
